049e6a5c2e148ecb29e100cb2452a0c1f8e0cf19
Soukaina Elhafif <soukainaelhafif@MacBook-Air-von-Soukaina.local>
2026-04-22 00:45:09 +0200

    Frontend final design fertig

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from backend.parsers.pdf_parser import extract_text_from_pdf, extract_skills
 from backend.nlp.skill_extractor import find_skill_gap, get_matching_skills, JOBMARKET_SKILLS
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
