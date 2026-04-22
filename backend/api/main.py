a2c9cd9b6871ebd036fc68c95550610b705fa10e
Soukaina Elhafif <souka.elhafif@gmail.com>
2026-04-22 17:45:28 +0200

    Fix CORS for GitHub Pages

--- backend/api/main.py
+++ backend/api/main.py
@@ -10,7 +10,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://soukainaelhafif.github.io", "*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
